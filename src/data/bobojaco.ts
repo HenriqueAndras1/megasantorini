9e8deab31a0d8cf60a411ab3a8d3a1c439b4b910
Henrique <119366049+HenriqueAndras1@users.noreply.github.com>
2025-05-15 18:57:30 -0300

    Ajuste nas imagens do projeto!

--- src/data/bobojaco.ts
+++ src/data/bobojaco.ts
@@ -1,16 +1,28 @@
 import { Produto } from './produtos';
 
 export const bobojaco: Produto[] = [
-  /* {
-    id: 17,
-    nome: 'Bobojaco Masculino Zara',
-    marca: 'Zara',
-    preco: 299.90,
-    estoque: 4,
-    tamanhos: ['M', 'G'],
-    imagem: '/produtos/bobojaco/zara.jpg',
-    logoMarca: '/marcas/zara-logo.png',
+   {
+    id: 701,
+    nome: 'Bobojaco Masculino Nike',
+    marca: 'Nike',
+    preco: 199.90,
+    estoque: 1,
+    tamanhos: ['G','G1','G2'],
+    imagem: '/produtos/bobojaco/bobojaco1.png',
+    logoMarca: '/marcas/nike-logo.png',
     categoria: 'bobojaco',
     linkShopee: 'https://shopee.com.br/bobojaco-masculino-zara',
-  }, */
+  },
+  {
+    id: 702,
+    nome: 'Bobojaco Masculino Nike',
+    marca: 'Nike',
+    preco: 199.90,
+    estoque: 1,
+    tamanhos: ['G','G1','G2'],
+    imagem: '/produtos/bobojaco/bobojaco2.png',
+    logoMarca: '/marcas/nike-logo.png',
+    categoria: 'bobojaco',
+    linkShopee: 'https://shopee.com.br/bobojaco-masculino-zara',
+  },
 ];
